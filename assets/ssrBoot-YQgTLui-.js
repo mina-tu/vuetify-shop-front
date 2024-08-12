e6053a0dbd722d7e2fa8ec3370fa1e6e5d12a123
mina-tu <164137181+mina-tu@users.noreply.github.com>
2024-08-12 07:36:01 +0000

    Deploying to gh-pages from @ mina-tu/vuetify-shop-front@d1a306009443a290027d01350b5c18386923ab6f 🚀

---
+++ assets/ssrBoot-YQgTLui-.js
@@ -0,0 +1,1 @@
+import{G as t,P as e,c as s,bX as n}from"./index-LLS26ABI.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
