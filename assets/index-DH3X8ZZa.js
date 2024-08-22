c00ff27e590644e0e64455a8fb385e61a554beb5
mina-tu <164137181+mina-tu@users.noreply.github.com>
2024-08-22 06:13:43 +0000

    Deploying to gh-pages from @ mina-tu/vuetify-shop-front@2d14e8cfba164bc84edfdf3642fd2e4641c60510 🚀

---
+++ assets/index-DH3X8ZZa.js
@@ -0,0 +1,1 @@
+import{_ as t,o as s,e as c}from"./index-i-EFj55S.js";import{b as e}from"./route-block-B_A1xBdJ.js";const _={class:"bg"},o={__name:"index",setup(a){return(n,r)=>(s(),c("div",_))}};typeof e=="function"&&e(o);const i=t(o,[["__scopeId","data-v-8e4ff6d9"]]);export{i as default};
